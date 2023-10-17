3dc2a26d74e11c648274609e91b5d31b109fdf33
Vanessa Tagliente <e227426@edu.sbl.ch>
2023-10-17 11:26:10 +0200

    Aufgaben 1-4 gelöst. A5 fast fertig

--- script.js
+++ script.js
@@ -4,5 +4,24 @@ function sayHello() {
 
 function changeBackground() {
   const body = document.querySelector("body")
-  body.style.backgroundColor = "darkgrey"
+  body.style.backgroundColor = "lightblue"
+}
+
+function deleteText() {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
 }
